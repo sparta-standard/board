96b28b091552b7897c36d1eb5387933470ffc452
choi-ys <project.log.062@gmail.com>
2025-03-31 11:19:04 +0900

    init: initial commit

---
+++ src/test/java/io/sparta/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sparta.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
