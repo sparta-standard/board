3956b42f3a6ee1aa658f99046b42d8ef9be86a2c
Jjini <sojinnuna@gmail.com>
2025-04-01 11:57:17 +0900

    test: commit 전 Test

--- src/main/java/io/sparta/board/BoardApplication.java
+++ src/main/java/io/sparta/board/BoardApplication.java
@@ -3,6 +3,7 @@ package io.sparta.board;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//수정
 @SpringBootApplication
 public class BoardApplication {
 
