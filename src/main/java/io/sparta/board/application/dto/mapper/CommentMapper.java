615c982689cf82aeb7c65ae26415e9b37ab265b8
dowon0113 <dowon1235@naver.com>
2025-04-02 02:01:24 +0900

    feat: comment 수정 - dto 변환작업 추가
    - 엔티티를 dto로 변환 로직 추가

--- src/main/java/io/sparta/board/application/dto/mapper/CommentMapper.java
+++ src/main/java/io/sparta/board/application/dto/mapper/CommentMapper.java
@@ -1,6 +1,7 @@
 package io.sparta.board.application.dto.mapper;
 
 import io.sparta.board.application.dto.response.CommentCreateResponseDto;
+import io.sparta.board.application.dto.response.comment.CommentUpdateResponseDto;
 import io.sparta.board.domain.entity.Comment;
 
 public class CommentMapper {
@@ -8,6 +9,12 @@ public class CommentMapper {
     public static CommentCreateResponseDto toCommentCreateResponseDto(Comment comment){
         return CommentCreateResponseDto.builder()
                 .id(comment.getId())
+                .content(comment.getContent())
+                .build();
+    }
+
+    public static CommentUpdateResponseDto toCommentUpdateResponseDto(Comment comment){
+        return CommentUpdateResponseDto.builder()
                 .content(comment.getContent())
                 .build();
     }
