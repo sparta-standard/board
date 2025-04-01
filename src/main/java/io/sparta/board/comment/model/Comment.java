a65cb17c4c51afd2a7ea103754259865105eb9f9
sohyun119 <sh-119@naver.com>
2025-04-01 23:58:03 +0900

    Comment Entity 생성

---
+++ src/main/java/io/sparta/board/comment/model/Comment.java
@@ -0,0 +1,64 @@
+package io.sparta.board.comment.model;
+
+import io.sparta.board.post.model.Post;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@Entity
+@Table(name = "p_comment")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id",  nullable = false)
+    private Post post;
+
+    @Column(length = 512,  nullable = false)
+    private String content;
+
+    @CreatedDate
+    @Column(name = "created_at",  updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @ColumnDefault("FALSE")
+    @Column(name = "is_deleted")
+    private boolean isDeleted;
+
+    public Comment(Post post, String content) {
+        this.post = post;
+        this.content = content;
+    }
+
+    public void deleteComment() {
+        this.isDeleted = true;
+        this.deletedAt = LocalDateTime.now();
+    }
+}
