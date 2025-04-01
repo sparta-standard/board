2e32f3c05a26ba12bd9d151b51d144f4b30bbdc0
AnJungGeon <gunn0128@gmail.com>
2025-04-01 15:37:28 +0900

    feat: 게시글 작성 repository 추가
    게시글 작성 repository 메소드 추가했습니다

--- src/main/java/io/sparta/board/model/post/repository/PostRepository.java
+++ src/main/java/io/sparta/board/model/post/repository/PostRepository.java
@@ -1,4 +1,7 @@
 package io.sparta.board.model.post.repository;
 
+import io.sparta.board.model.post.entity.Post;
+
 public interface PostRepository {
+    Post save(Post post);
 }
--- src/main/java/io/sparta/board/model/post/repository/PostRepositoryImpl.java
+++ src/main/java/io/sparta/board/model/post/repository/PostRepositoryImpl.java
@@ -1,7 +1,17 @@
 package io.sparta.board.model.post.repository;
 
+import io.sparta.board.model.post.entity.Post;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class PostRepositoryImpl implements PostRepository{
+
+    private final PostJpaRepository postJpaRepository;
+
+    @Override
+    public Post save(Post post) {
+        return postJpaRepository.save(post);
+    }
 }
