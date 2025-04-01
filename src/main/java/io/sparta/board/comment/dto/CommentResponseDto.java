d5e9de0997f75c6f2d147e5167bb1b764c589911
mummumm <01088287493@daum.net>
2025-04-02 00:10:51 +0900

    FEAT: Post, Comment entity, dto 설정 - Post, PostRequestDto, PostUpdateRequestDto, PostResponseDto 생성 - Comment, CommentRequestDto, CommentUpdateRequestDto, CommentResponseDto 생성 - BoardApplication.java에 audit 어노테이션 추가

---
+++ src/main/java/io/sparta/board/comment/dto/CommentResponseDto.java
@@ -0,0 +1,33 @@
+package io.sparta.board.comment.dto;
+
+import io.sparta.board.comment.model.Comment;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentResponseDto {
+
+    private UUID id;
+    private UUID postId;
+    private String content;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private boolean deleted;
+
+    public CommentResponseDto(Comment comment) {
+        this.id = comment.getId();
+        this.postId = comment.getPost().getId();
+        this.content = comment.getContent();
+
+        this.createdAt = comment.getCreatedAt();
+        this.updatedAt = comment.getUpdatedAt();
+        this.deleted = comment.isDeleted();
+    }
+}
---
+++ src/main/java/io/sparta/board/comment/dto/CommentUpdateRequestDto.java
@@ -0,0 +1,14 @@
+package io.sparta.board.comment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentUpdateRequestDto {
+
+    private String content;
+
+}
