d3a3a0fb44dbe10d704479cc0acb2233c9f3de3f
Imnotcoderdude <topsolo7@naver.com>
2025-04-01 11:51:22 +0900

    fix : 간단하게 제작하기 위해 레포지토리 구현체 제거 - BoardRepository에 직접 JpaRepository 상속받아서 사용.

--- src/main/java/io/sparta/board/board/infrastructure/jpa/BoardJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package io.sparta.board.board.infrastructure.jpa;
-
-import io.sparta.board.board.domain.entity.Board;
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardJpaRepository extends JpaRepository<Board, UUID> {
-
-}
