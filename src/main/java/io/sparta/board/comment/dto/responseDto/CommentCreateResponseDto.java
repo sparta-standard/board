0ff1e44e3bb3da67f1a410db94c840819d4d64a4
sohyun119 <sh-119@naver.com>
2025-04-02 00:22:38 +0900

    댓글 등록 Response DTO 생성

---
+++ src/main/java/io/sparta/board/comment/dto/responseDto/CommentCreateResponseDto.java
@@ -0,0 +1,23 @@
+package io.sparta.board.comment.dto.responseDto;
+
+import io.sparta.board.comment.model.Comment;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.Getter;
+
+@Getter
+public class CommentCreateResponseDto {
+
+    private UUID id;
+    private UUID postId;
+    private String comment;
+    private LocalDateTime createdAt;
+
+    public CommentCreateResponseDto(Comment comment) {
+        this.id = comment.getId();
+        this.postId = comment.getPost().getId();
+        this.comment = comment.getContent();
+        this.createdAt = comment.getCreatedAt();
+    }
+
+}
