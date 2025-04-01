d086d9133c51c97bda3eb6ee04de523781de473c
dydwo6018 <dydwo6018@gmail.com>
2025-04-01 19:15:45 +0900

    feat: PostCreateResponseDto 생성

---
+++ src/main/java/io/sparta/board/dto/PostCreateResponseDto.java
@@ -0,0 +1,26 @@
+package io.sparta.board.dto;
+
+import io.sparta.board.entity.Post;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor
+public class PostCreateResponseDto {
+    private UUID id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+
+
+    public PostCreateResponseDto(Post post) {
+        this.id = post.getId();
+        this.title = post.getTitle();
+        this.content = post.getContent();
+        this.createdAt = post.getCreatedAt();
+
+    }
+}
