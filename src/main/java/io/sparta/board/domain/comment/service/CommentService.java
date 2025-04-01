09c649d5feb12b988d23c374fd66432e82f3fc24
AnJungGeon <gunn0128@gmail.com>
2025-04-01 23:17:59 +0900

    feat: 댓글 삭제, 댓글 조회 비즈니스 로직 추가했습니다
    - 댓글 삭제 비즈니스 로직 추가했습니다
    - 게시글 조회 시 댓글 리스트를 조회하고 삭제 되지 않은 댓글을 리스트에 추가하는 메소드 추가했습니다

--- src/main/java/io/sparta/board/domain/comment/service/CommentService.java
+++ src/main/java/io/sparta/board/domain/comment/service/CommentService.java
@@ -11,6 +11,8 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -58,5 +60,28 @@ public class CommentService {
                 .postId(updateComment.getPost().getId())
                 .content(updateComment.getContent())
                 .build();
+    }
+
+    public void deleteComment(UUID id) {
+
+        Comment existComment = commentReposiroy.findByIdAndDeletedIsFalse(id)
+                .orElseThrow(() -> new IllegalArgumentException("이미 삭제된 댓글"));
+
+        existComment.setDeleted(true);
+
+        commentReposiroy.save(existComment);
+
+    }
+
+    public List<Comment> getPostComment(UUID id) {
+
+        List<Comment> comments = new ArrayList<>();
+        List<Comment> commentList = commentReposiroy.findByPostIdAndDeletedIsFalse(id);
+
+        for (Comment comment : commentList) {
+            comments.add(comment);
+        }
+
+        return comments;
     }
 }
