1985400a53e7cdd3eb366379984a6d667ca1b832
adgjl <adgjl0122@naver.com>
2025-04-01 20:53:13 +0900

    feat: Post Delete Controller 작성

--- src/main/java/io/sparta/board/app/domain/post/presentation/controller/PostController.java
+++ src/main/java/io/sparta/board/app/domain/post/presentation/controller/PostController.java
@@ -4,10 +4,12 @@ import io.sparta.board.app.domain.post.application.facade.PostFacade;
 import io.sparta.board.app.domain.post.presentation.dto.request.PostCreateRequestDto;
 import io.sparta.board.app.domain.post.presentation.dto.request.PostUpdateRequestDto;
 import io.sparta.board.app.domain.post.presentation.dto.response.PostCreatResponseDto;
+import io.sparta.board.app.domain.post.presentation.dto.response.PostDeleteResponseDto;
 import io.sparta.board.app.domain.post.presentation.dto.response.PostUpdateResponseDto;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,6 +32,11 @@ public class PostController {
 	public ResponseEntity<PostUpdateResponseDto> updatePost(@PathVariable("id") UUID id, @RequestBody
 		PostUpdateRequestDto postUpdateRequestDto) {
 		return ResponseEntity.ok(postFacade.updatePost(id, postUpdateRequestDto));
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<PostDeleteResponseDto> deletePost(@PathVariable("id") UUID id) {
+		return ResponseEntity.ok(postFacade.deletePost(id));
 	}
 
 }
