8e3963b9b74a4a3e25e88f5db70c69e17d0b094b
AnJungGeon <gunn0128@gmail.com>
2025-04-01 16:05:15 +0900

    feat: 게시글 조회 요청 메소드 작성
    게시글 조회 컨트롤러에 요청 메소드 작성했습니다

--- src/main/java/io/sparta/board/domain/post/controller/PostController.java
+++ src/main/java/io/sparta/board/domain/post/controller/PostController.java
@@ -1,15 +1,15 @@
 package io.sparta.board.domain.post.controller;
 
 import io.sparta.board.domain.post.dto.request.PostCreateRequestDto;
+import io.sparta.board.domain.post.dto.response.GetPostResponseDto;
 import io.sparta.board.domain.post.dto.response.PostResponseDto;
 import io.sparta.board.domain.post.service.PostServcie;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/post")
@@ -35,5 +35,25 @@ public class PostController {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
+
+
+    //게시글 단건 조회
+    @GetMapping("/{id}")
+    public ResponseEntity<GetPostResponseDto> getPost(
+            @PathVariable UUID id
+    ){
+        if(id == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+
+        GetPostResponseDto responseDto = postService.getPost(id);
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
+    }
+
+
+
+
+
 
 }
