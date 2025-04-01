1123d6393031dd0a6f5823f6cee39c1d66eb7eae
sohyun119 <sh-119@naver.com>
2025-04-01 23:33:06 +0900

    게시물 단일 조회 (상세 조회)시 응답 DTO 생성

---
+++ src/main/java/io/sparta/board/post/dto/responseDto/PostDetailsResponseDto.java
@@ -0,0 +1,31 @@
+package io.sparta.board.post.dto.responseDto;
+
+import io.sparta.board.comment.dto.responseDto.CommentResponseDto;
+import io.sparta.board.post.model.Post;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+
+@AllArgsConstructor
+public class PostDetailsResponseDto {
+
+    private final UUID id;
+    private final String title;
+    private final String content;
+    private final Long count;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime updatedAt;
+    private final Page<CommentResponseDto> comments;
+
+    public PostDetailsResponseDto(Post post, Page<CommentResponseDto> comments) {
+        this.id = post.getId();
+        this.title = post.getTitle();
+        this.content = post.getContent();
+        this.count = post.getCount();
+        this.createdAt = post.getCreatedAt();
+        this.updatedAt = post.getUpdatedAt();
+        this.comments = comments;
+    }
+
+}
