0eb1897b75fd094a759b25c0480491a88a979995
dowon0113 <dowon1235@naver.com>
2025-04-02 00:46:26 +0900

    feat: post 조회 비즈니스 로직 구현
    - 삭제된 게시글은 조회 불가 > 예외처리

--- src/main/java/io/sparta/board/application/service/PostService.java
+++ src/main/java/io/sparta/board/application/service/PostService.java
@@ -3,11 +3,14 @@ package io.sparta.board.application.service;
 import io.sparta.board.application.dto.mapper.PostMapper;
 import io.sparta.board.application.dto.request.PostCreateRequestDto;
 import io.sparta.board.application.dto.response.PostCreateResponseDto;
+import io.sparta.board.application.dto.response.PostGetResponseDto;
 import io.sparta.board.domain.entity.Post;
 import io.sparta.board.domain.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -22,5 +25,17 @@ public class PostService {
         PostCreateResponseDto response = PostMapper.toPostCreateResponseDto(savedPost);
 
         return response;
+    }
+
+    @Transactional(readOnly = true)
+    public PostGetResponseDto getDetailPost(UUID id){
+
+        Post post = postRepository.findByPostId(id);
+
+        if(post.isDeleted()){
+            throw new IllegalArgumentException("삭제된 게시글입니다.");
+        }
+
+        return PostMapper.toPostGetResponseDto(post);
     }
 }
