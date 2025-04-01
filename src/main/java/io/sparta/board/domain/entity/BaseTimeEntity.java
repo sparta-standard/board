f0725dec2f77e64e85dd5d39ea41ef5608f27dd9
Sohae Park <ipnsohe@naver.com>
2025-04-02 01:23:56 +0900

    feat: BaseTimeEntity 구현 및 @EnableJpaAuditing 어노테이션 추가
    - 모든 데이터의 생성/수정/삭제 시 변경 시점에 대한 데이터가 자동으로 관리 되도록 JPA Audit 기능 설정

---
+++ src/main/java/io/sparta/board/domain/entity/BaseTimeEntity.java
@@ -0,0 +1,32 @@
+package io.sparta.board.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(name="created_at", nullable = false)
+    private LocalDateTime createAt;
+
+    @LastModifiedDate
+    @Column(name="updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name="deleted")
+    private Boolean deleted = false;
+
+    public void deletedOf() {
+        this.deleted = true;
+    }
+}
