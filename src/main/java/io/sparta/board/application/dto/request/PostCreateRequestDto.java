d88a923e2c1e1446915f6d704d33ccc7c2652cc2
dowon0113 <dowon1235@naver.com>
2025-04-01 21:05:59 +0900

    feat: dto 생성
    - responseDto : 응답 dto 필드값 설정 (id, title, content)
    - requestDto : 요청 dto 필드값 설정, entity 변환 메서드 추가

---
+++ src/main/java/io/sparta/board/application/dto/request/PostCreateRequestDto.java
@@ -0,0 +1,24 @@
+package io.sparta.board.application.dto.request;
+
+import io.sparta.board.domain.entity.Post;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PostCreateRequestDto {
+    private String title;
+    private String content;
+
+    public Post toEntity() {
+        return Post.create(
+                title,
+                content
+        );
+    }
+
+}
