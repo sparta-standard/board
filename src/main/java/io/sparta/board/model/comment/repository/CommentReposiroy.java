acd2113363cb9f517aad9204a8797a44e7c956ed
AnJungGeon <gunn0128@gmail.com>
2025-04-01 19:45:57 +0900

    feat: 댓글 생성 repository 메소드 작성
    댓글 생성 repository 메소드 작성했습니다

--- src/main/java/io/sparta/board/model/comment/repository/CommentReposiroy.java
+++ src/main/java/io/sparta/board/model/comment/repository/CommentReposiroy.java
@@ -1,4 +1,7 @@
 package io.sparta.board.model.comment.repository;
 
+import io.sparta.board.model.comment.entity.Comment;
+
 public interface CommentReposiroy {
+    Comment save(Comment comment);
 }
--- src/main/java/io/sparta/board/model/comment/repository/CommentRepositoryImpl.java
+++ src/main/java/io/sparta/board/model/comment/repository/CommentRepositoryImpl.java
@@ -1,7 +1,17 @@
 package io.sparta.board.model.comment.repository;
 
+import io.sparta.board.model.comment.entity.Comment;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class CommentRepositoryImpl implements CommentReposiroy{
+
+    private final CommentJpaRepository commentJpaRepository;
+
+    @Override
+    public Comment save(Comment comment) {
+        return commentJpaRepository.save(comment);
+    }
 }
