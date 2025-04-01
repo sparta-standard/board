a7aeab35bbb9ce5e012221d72dede0dc204a222b
dowon0113 <dowon1235@naver.com>
2025-04-02 00:42:34 +0900

    feat: Post 단건 조회 api 구현
    - 게시글 조회 엔드포인트 추가

--- src/main/java/io/sparta/board/presentation/PostController.java
+++ src/main/java/io/sparta/board/presentation/PostController.java
@@ -2,13 +2,13 @@ package io.sparta.board.presentation;
 
 import io.sparta.board.application.dto.request.PostCreateRequestDto;
 import io.sparta.board.application.dto.response.PostCreateResponseDto;
+import io.sparta.board.application.dto.response.PostGetResponseDto;
 import io.sparta.board.application.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,6 +21,14 @@ public class PostController {
     public ResponseEntity<PostCreateResponseDto> createPost(
             @RequestBody PostCreateRequestDto request) {
         PostCreateResponseDto response = postService.createPost(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PostGetResponseDto> GetPost(
+            @PathVariable UUID id){
+
+        PostGetResponseDto response = postService.getDetailPost(id);
         return ResponseEntity.ok(response);
     }
 }
