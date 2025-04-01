c0b2580aa8ec6ba094cf5381e4622f66c58be8a2
yejiscore <yejisong24@gmail.com>
2025-04-02 00:34:37 +0900

    feat: 댓글 Entity 구현

---
+++ src/main/java/io/sparta/board/domain/entity/Comment.java
@@ -0,0 +1,33 @@
+package io.sparta.board.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "p_comment")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID commentId;
+
+    @Column(nullable = false)
+    private String commentContent;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @Builder
+    public Comment(String commentContent, Post post) {
+        this.commentContent = commentContent;
+        this.post = post;
+    }
+}
