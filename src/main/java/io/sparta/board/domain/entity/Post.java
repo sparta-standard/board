918fdb6180a4187cab9d3462769d81978fa93589
yejiscore <yejisong24@gmail.com>
2025-04-02 00:33:24 +0900

    feat: 게시글 Entity 구현

---
+++ src/main/java/io/sparta/board/domain/entity/Post.java
@@ -0,0 +1,37 @@
+package io.sparta.board.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "p_post")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID postId;
+
+    @Column(nullable = false)
+    private String postTitle;
+
+    @Column(nullable = false)
+    private String postContent;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
+    private List<Comment> comments = new ArrayList<>();
+
+    @Builder
+    public Post(String postTitle, String postContent) {
+        this.postTitle = postTitle;
+        this.postContent = postContent;
+    }
+}
