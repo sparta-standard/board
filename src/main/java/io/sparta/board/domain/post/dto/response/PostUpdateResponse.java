a3b77502c9ebfc0349b0e252a88cc43fb20d6ab3
AnJungGeon <gunn0128@gmail.com>
2025-04-01 21:27:54 +0900

    feat: 게시글 수정 요청, 응답 dto 생성
    게시글 수정 요청 요청, 응답 dto 생성

---
+++ src/main/java/io/sparta/board/domain/post/dto/response/PostUpdateResponse.java
@@ -0,0 +1,14 @@
+package io.sparta.board.domain.post.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+@Builder
+public class PostUpdateResponse {
+    private UUID id;
+    private String title;
+    private String content;
+}
