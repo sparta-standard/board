0bdfcd097a51b8349042f7ff82481030e42ff9bc
Imnotcoderdude <topsolo7@naver.com>
2025-04-01 14:46:16 +0900

    feat : board 생성 기능 추가 - @EnableJpaAuditing 작성으로 생성,수정일자 자동 생성 구현 - Builder 패턴으로 엔티티 생성

--- src/main/java/io/sparta/board/board/application/service/BoardService.java
+++ src/main/java/io/sparta/board/board/application/service/BoardService.java
@@ -1,5 +1,8 @@
 package io.sparta.board.board.application.service;
 
+import io.sparta.board.board.application.dto.request.BoardCreateRequestDto;
+import io.sparta.board.board.application.dto.response.BoardCreateResponseDto;
+import io.sparta.board.board.domain.entity.Board;
 import io.sparta.board.board.domain.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,5 +12,20 @@ import org.springframework.stereotype.Service;
 public class BoardService {
 
     private final BoardRepository boardRepository;
+
+    public BoardCreateResponseDto createBoard(BoardCreateRequestDto requestDto) {
+        Board board = Board.builder()
+            .title(requestDto.getTitle())
+            .content(requestDto.getContent())
+            .build();
+
+        boardRepository.save(board);
+
+        return BoardCreateResponseDto.builder()
+            .boardId(board.getId())
+            .title(board.getTitle())
+            .content(board.getContent())
+            .build();
+    }
 
 }
--- src/main/java/io/sparta/board/board/presentation/controller/BoardController.java
+++ src/main/java/io/sparta/board/board/presentation/controller/BoardController.java
@@ -1,14 +1,31 @@
 package io.sparta.board.board.presentation.controller;
 
+import io.sparta.board.board.application.dto.request.BoardCreateRequestDto;
+import io.sparta.board.board.application.dto.response.BoardCreateResponseDto;
 import io.sparta.board.board.application.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class BoardController {
 
     private final BoardService boardService;
+
+    @PostMapping("/boards")
+    public ResponseEntity<BoardCreateResponseDto> createBoard(
+        @RequestBody BoardCreateRequestDto requestDto
+    ) {
+        BoardCreateResponseDto responseDto = boardService.createBoard(requestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+    }
+
+
 
 }
