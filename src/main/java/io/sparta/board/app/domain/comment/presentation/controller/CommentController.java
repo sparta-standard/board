cdbb4b95cb3570b9dd5e1b2d4646bf4f46b562fe
adgjl <adgjl0122@naver.com>
2025-04-01 22:29:55 +0900

    feat: Comment Update Controller 작성

--- src/main/java/io/sparta/board/app/domain/comment/presentation/controller/CommentController.java
+++ src/main/java/io/sparta/board/app/domain/comment/presentation/controller/CommentController.java
@@ -1,11 +1,15 @@
 package io.sparta.board.app.domain.comment.presentation.controller;
 
 import io.sparta.board.app.domain.comment.application.facade.CommentFacade;
-import io.sparta.board.app.domain.comment.application.usecase.CommentService;
 import io.sparta.board.app.domain.comment.presentation.dto.request.CommentCreateRequestDto;
+import io.sparta.board.app.domain.comment.presentation.dto.request.CommentUpdateRequestDto;
 import io.sparta.board.app.domain.comment.presentation.dto.response.CommentCreateResponseDto;
+import io.sparta.board.app.domain.comment.presentation.dto.response.CommentUpdateResponseDto;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +26,11 @@ public class CommentController {
 	public ResponseEntity<CommentCreateResponseDto> CreateComment(@RequestBody
 		CommentCreateRequestDto commentCreateRequestDto) {
 		return ResponseEntity.ok(commentFacade.createComment(commentCreateRequestDto));
+	}
+
+	@PatchMapping("/{id}")
+	public ResponseEntity<CommentUpdateResponseDto> UpdateComment(@PathVariable("id") UUID id, @RequestBody
+	CommentUpdateRequestDto commentUpdateRequestDto) {
+		return ResponseEntity.ok(commentFacade.updateComment(id, commentUpdateRequestDto));
 	}
 }
