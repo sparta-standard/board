89e6dbd6b91f09b7c28405d26e5b229717f4688a
Imnotcoderdude <topsolo7@naver.com>
2025-04-01 20:15:22 +0900

    feat : 댓글 생성 기능 추가 - 테스트 완료 - boardService 의존성 주입 받아서 사용.

--- src/main/java/io/sparta/board/comment/application/service/CommentService.java
+++ src/main/java/io/sparta/board/comment/application/service/CommentService.java
@@ -1,6 +1,12 @@
 package io.sparta.board.comment.application.service;
 
+import io.sparta.board.board.application.service.BoardService;
+import io.sparta.board.board.domain.entity.Board;
+import io.sparta.board.comment.application.dto.request.CommentCreateRequestDto;
+import io.sparta.board.comment.application.dto.response.CommentCreateResponseDto;
+import io.sparta.board.comment.domain.entity.Comment;
 import io.sparta.board.comment.domain.repository.CommentRepository;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +15,22 @@ import org.springframework.stereotype.Service;
 public class CommentService {
 
     private final CommentRepository commentRepository;
+    private final BoardService boardService;
+
+    public CommentCreateResponseDto createComment(UUID boardId, CommentCreateRequestDto requestDto) {
+        Board board = boardService.findBoard(boardId);
+
+        Comment comment = Comment.builder()
+            .board(board)
+            .content(requestDto.getContent())
+            .build();
+
+        commentRepository.save(comment);
+
+        return CommentCreateResponseDto.builder()
+            .boardId(board.getId())
+            .commentId(comment.getId())
+            .content(comment.getContent())
+            .build();
+    }
 }
--- src/main/java/io/sparta/board/comment/presentation/controller/CommentController.java
+++ src/main/java/io/sparta/board/comment/presentation/controller/CommentController.java
@@ -1,12 +1,32 @@
 package io.sparta.board.comment.presentation.controller;
 
+import io.sparta.board.comment.application.dto.request.CommentCreateRequestDto;
+import io.sparta.board.comment.application.dto.response.CommentCreateResponseDto;
 import io.sparta.board.comment.application.service.CommentService;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/boards/")
 public class CommentController {
 
     private final CommentService commentService;
+
+    @PostMapping("/{boardId}/comments")
+    public ResponseEntity<CommentCreateResponseDto> createComment(
+        @PathVariable(name = "boardId") UUID boardId,
+        @RequestBody CommentCreateRequestDto requestDto
+    ) {
+        CommentCreateResponseDto responseDto = commentService.createComment(boardId, requestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+    }
+
 }
