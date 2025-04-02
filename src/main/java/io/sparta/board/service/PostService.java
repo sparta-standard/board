e9d95fa5d82e3cce3dc1f3d76386bda435574c5e
dydwo6018 <dydwo6018@gmail.com>
2025-04-02 12:21:13 +0900

    softDelete 코드 수정

--- src/main/java/io/sparta/board/service/PostService.java
+++ src/main/java/io/sparta/board/service/PostService.java
@@ -56,7 +56,7 @@ public class PostService {
         Post post = postRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글을 찾을 수 없습니다."));
 
-        post.setDeleted(true);
+        post.softDelete();
 
     }
 
