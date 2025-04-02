f53a9e89e2864ab57e21edc05fcb71c30226acde
oh-min <ohyemin96@gmail.com>
2025-04-02 11:53:18 +0900

    Feat : Post Entity 추가
    - title, content, deleted 등 필드 정의
    - created_at/updated_at 자동 설정을 위한 JPA Auditing 적용

---
+++ src/main/java/io/sparta/board/domain/entity/Post.java
@@ -0,0 +1,50 @@
+package io.sparta.board.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.sql.Timestamp;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@Table(name = "p_post")
+@EntityListeners(AuditingEntityListener.class)
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  @Column(name = "id", nullable = false)
+  private UUID id;
+
+  @Column(name = "title", nullable = false)
+  private String title;
+
+  @Column(name = "content", nullable = false)
+  private String content;
+
+  @Column(name = "deleted")
+  private Boolean deleted = false;
+
+  @CreatedDate
+  @Column(name = "created_at", updatable = false, nullable = false)
+  private Timestamp createdAt;
+
+  @LastModifiedDate
+  @Column(name = "updated_at")
+  private Timestamp updatedAt;
+}
