f37d65725dcde3d66c08a40f4511c264d253c1c5
문고은 <>
2025-04-02 11:26:22 +0900

    feat: 삭제된 게시글 조회/수정/삭제 제한

--- src/main/java/io/sparta/board/application/usecase/PostServiceImpl.java
+++ src/main/java/io/sparta/board/application/usecase/PostServiceImpl.java
@@ -22,19 +22,29 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public Post getPost(UUID postId) {
-        return postRepository.findById(postId)
+        Post post = postRepository.findById(postId)
             .orElseThrow(() -> new IllegalArgumentException("게시글이 존재하지 않습니다."));
+        isDeleted(post);
+        return post;
     }
 
     @Transactional
     @Override
     public void updatePost(Post post, PostUpdateRequestDto requestDto) {
+        isDeleted(post);
         requestDto.updatePost(post);
     }
 
     @Transactional
     @Override
     public void deletePost(Post post) {
+        isDeleted(post);
         post.delete();
+    }
+
+    private void isDeleted(Post post) {
+        if (post.isDeleted()) {
+            throw new IllegalArgumentException("삭제된 게시글입니다.");
+        }
     }
 }
--- src/main/java/io/sparta/board/model/entity/BaseEntity.java
+++ src/main/java/io/sparta/board/model/entity/BaseEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -20,6 +21,7 @@ public class BaseEntity {
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
+    @Getter
     @Column(nullable = false)
     private boolean deleted = Boolean.FALSE;
 
