58038692c1137f6a400804f6e899a800b29db14a
문고은 <>
2025-04-02 10:16:51 +0900

    feat: 게시글 단건 조회 API 생성

--- src/main/java/io/sparta/board/presentation/controller/PostController.java
+++ src/main/java/io/sparta/board/presentation/controller/PostController.java
@@ -4,8 +4,11 @@ import io.sparta.board.application.facade.PostFacade;
 import io.sparta.board.presentation.dto.request.PostCreateRequestDto;
 import io.sparta.board.presentation.dto.response.PostResponseDto;
 import jakarta.validation.Valid;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,5 +27,12 @@ public class PostController {
         @RequestBody PostCreateRequestDto requestDto
     ) {
         return ResponseEntity.ok(postFacade.createPost(requestDto));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PostResponseDto> getPost(
+        @PathVariable("id") UUID postId
+    ) {
+        return ResponseEntity.ok(postFacade.getPost(postId));
     }
 }
