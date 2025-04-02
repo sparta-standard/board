4a50b410cbfbf69538ac816022a687db7e64024d
문고은 <>
2025-04-02 10:16:26 +0900

    feat: 게시글 단건 조회 facade 구현

--- src/main/java/io/sparta/board/application/facade/PostFacade.java
+++ src/main/java/io/sparta/board/application/facade/PostFacade.java
@@ -1,10 +1,12 @@
 package io.sparta.board.application.facade;
 
-import io.sparta.board.presentation.dto.PostCreateRequestDto;
-import io.sparta.board.presentation.dto.PostResponseDto;
 import io.sparta.board.presentation.dto.request.PostCreateRequestDto;
 import io.sparta.board.presentation.dto.response.PostResponseDto;
+import java.util.UUID;
 
 public interface PostFacade {
+
     PostResponseDto createPost(PostCreateRequestDto requestDto);
+
+    PostResponseDto getPost(UUID postId);
 }
--- src/main/java/io/sparta/board/application/facade/PostFacadeImpl.java
+++ src/main/java/io/sparta/board/application/facade/PostFacadeImpl.java
@@ -2,10 +2,9 @@ package io.sparta.board.application.facade;
 
 import io.sparta.board.application.usecase.PostService;
 import io.sparta.board.model.entity.Post;
-import io.sparta.board.presentation.dto.PostCreateRequestDto;
-import io.sparta.board.presentation.dto.PostResponseDto;
 import io.sparta.board.presentation.dto.request.PostCreateRequestDto;
 import io.sparta.board.presentation.dto.response.PostResponseDto;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -19,5 +18,11 @@ public class PostFacadeImpl implements PostFacade {
     public PostResponseDto createPost(PostCreateRequestDto requestDto) {
         Post savedPost = postService.createPost(requestDto.createPost());
         return PostResponseDto.toResponseDto(savedPost);
+    }
+
+    @Override
+    public PostResponseDto getPost(UUID postId) {
+        Post post = postService.getPost(postId);
+        return PostResponseDto.toResponseDto(post);
     }
 }
