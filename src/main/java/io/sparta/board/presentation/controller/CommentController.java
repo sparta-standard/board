1c2c8cfc81379b24bb1be709df5f2750f70562d9
문고은 <>
2025-04-02 12:03:28 +0900

    feat: 댓글 수정 API 생성

--- src/main/java/io/sparta/board/presentation/controller/CommentController.java
+++ src/main/java/io/sparta/board/presentation/controller/CommentController.java
@@ -2,11 +2,13 @@ package io.sparta.board.presentation.controller;
 
 import io.sparta.board.application.facade.CommentFacade;
 import io.sparta.board.presentation.dto.request.CommentCreateRequestDto;
+import io.sparta.board.presentation.dto.request.CommentUpdateRequestDto;
 import io.sparta.board.presentation.dto.response.CommentResponseDto;
 import jakarta.validation.Valid;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,5 +29,14 @@ public class CommentController {
         @PathVariable("id") UUID postId
     ) {
         return ResponseEntity.ok(commentFacade.createComment(requestDto, postId));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<CommentResponseDto> updateComment(
+        @Valid
+        @RequestBody CommentUpdateRequestDto requestDto,
+        @PathVariable("id") UUID commentId
+    ) {
+        return ResponseEntity.ok(commentFacade.updateComment(commentId, requestDto));
     }
 }
