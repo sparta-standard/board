f283982db38e440ff8cd992ac2c22a1ac4c72c9a
문고은 <>
2025-04-02 12:17:36 +0900

    feat: 댓글 삭제 API 생성

--- src/main/java/io/sparta/board/presentation/controller/CommentController.java
+++ src/main/java/io/sparta/board/presentation/controller/CommentController.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,5 +39,12 @@ public class CommentController {
         @PathVariable("id") UUID commentId
     ) {
         return ResponseEntity.ok(commentFacade.updateComment(commentId, requestDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteComment(
+        @PathVariable("id") UUID commentId
+    ) {
+        commentFacade.deleteComment(commentId);
     }
 }
