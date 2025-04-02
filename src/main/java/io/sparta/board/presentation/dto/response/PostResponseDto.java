7c0e566cb75e89c8f4e31de7fc3db3fd85055ca2
문고은 <>
2025-04-02 12:49:21 +0900

    feat: 게시글 응답 DTO에 댓글 리스트 추가

--- src/main/java/io/sparta/board/presentation/dto/response/PostResponseDto.java
+++ src/main/java/io/sparta/board/presentation/dto/response/PostResponseDto.java
@@ -2,6 +2,7 @@ package io.sparta.board.presentation.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.sparta.board.model.entity.Post;
+import java.util.List;
 import java.util.UUID;
 import lombok.Builder;
 
@@ -14,12 +15,15 @@ public class PostResponseDto {
     private final String title;
     @JsonProperty
     private final String content;
+    @JsonProperty
+    private final List<CommentResponseDto> comments;
 
-    public static PostResponseDto toResponseDto(Post post) {
+    public static PostResponseDto toResponseDto(Post post, List<CommentResponseDto> comments) {
         return PostResponseDto.builder()
             .id(post.getId())
             .title(post.getTitle())
             .content(post.getContent())
+            .comments(comments)
             .build();
     }
 }
