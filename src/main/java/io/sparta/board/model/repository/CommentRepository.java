23a85fda9a1e506f5a4335cdf10f15260d5e1203
문고은 <>
2025-04-02 12:02:04 +0900

    feat: 댓글 아이디 기반 조회 repository 구현

--- src/main/java/io/sparta/board/model/repository/CommentRepository.java
+++ src/main/java/io/sparta/board/model/repository/CommentRepository.java
@@ -1,7 +1,12 @@
 package io.sparta.board.model.repository;
 
 import io.sparta.board.model.entity.Comment;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface CommentRepository {
+
     Comment save(Comment comment);
+
+    Optional<Comment> findById(UUID commentId);
 }
