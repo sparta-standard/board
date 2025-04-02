825d6fab2992a01710293364d9512cfbb60cffd1
yejiscore <yejisong24@gmail.com>
2025-04-02 11:25:46 +0900

    feat: 유효성 검사 어노테이션 추가

--- src/main/java/io/sparta/board/post/presentation/PostController.java
+++ src/main/java/io/sparta/board/post/presentation/PostController.java
@@ -4,6 +4,7 @@ import io.sparta.board.post.application.dto.request.PostRequestDto;
 import io.sparta.board.post.application.dto.response.PostListResponseDto;
 import io.sparta.board.post.application.dto.response.PostResponseDto;
 import io.sparta.board.post.application.service.PostService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,7 +19,7 @@ public class PostController {
     private final PostService postService;
 
     @PostMapping
-    public ResponseEntity<PostResponseDto> createPost(@RequestBody PostRequestDto requestDto) {
+    public ResponseEntity<PostResponseDto> createPost(@RequestBody @Valid PostRequestDto requestDto) {
         PostResponseDto response = postService.createPost(requestDto);
         return ResponseEntity.ok(response);
     }
@@ -36,7 +37,7 @@ public class PostController {
     @PutMapping("/{postId}")
     public ResponseEntity<PostResponseDto> updatePost(
             @PathVariable UUID postId,
-            @RequestBody PostRequestDto requestDto) {
+            @RequestBody @Valid PostRequestDto requestDto) {
         return ResponseEntity.ok(postService.updatePost(postId, requestDto));
     }
 }
