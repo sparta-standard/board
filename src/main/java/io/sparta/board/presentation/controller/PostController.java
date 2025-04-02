9140ebe58c538dee4570e5afd5aa1b6ea03fb034
문고은 <>
2025-04-02 11:12:48 +0900

    feat: 게시글 삭제 API 생성

--- src/main/java/io/sparta/board/presentation/controller/PostController.java
+++ src/main/java/io/sparta/board/presentation/controller/PostController.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -45,5 +46,12 @@ public class PostController {
         @PathVariable("id") UUID postId
     ) {
         return ResponseEntity.ok(postFacade.updatePost(postId, requestDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePost(
+        @PathVariable("id") UUID postId
+    ) {
+        postFacade.deletePost(postId);
     }
 }
