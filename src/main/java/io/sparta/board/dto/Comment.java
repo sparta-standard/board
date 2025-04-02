fdb38c9547e0d073281d381d456f5b6c7ae9b413
Jjini <sojinnuna@gmail.com>
2025-04-02 12:50:02 +0900

    [feat] dto 생성

---
+++ src/main/java/io/sparta/board/dto/Comment.java
@@ -0,0 +1,29 @@
+package io.sparta.board.dto;
+
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "p_comment")
+@NoArgsConstructor
+public class Comment extends BaseEntity {
+
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="post_id", nullable=false)
+    private Post post;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private boolean deleted = false;
+
+}
---
+++ src/main/java/io/sparta/board/dto/Post.java
@@ -0,0 +1,27 @@
+package io.sparta.board.dto;
+
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "p_post")
+@NoArgsConstructor
+public class Post extends BaseEntity {
+
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private boolean deleted = false;
+
+}
