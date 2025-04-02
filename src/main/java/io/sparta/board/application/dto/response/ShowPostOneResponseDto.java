460e37b13cdbc688b6663d0347a921be0b52726f
goodperiodt <ilypsj@gmail.com>
2025-04-02 12:42:05 +0900

    feat: ShowPostOneResponseDto 내 필드 조정으로 게시물 조회시 해당 게시물에 작성된 댓글 조회 구현

--- src/main/java/io/sparta/board/application/dto/response/ShowPostOneResponseDto.java
+++ src/main/java/io/sparta/board/application/dto/response/ShowPostOneResponseDto.java
@@ -1,9 +1,12 @@
 package io.sparta.board.application.dto.response;
 
+import io.sparta.board.domain.model.Comment;
 import io.sparta.board.domain.model.Post;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Getter
@@ -17,7 +20,7 @@ public class ShowPostOneResponseDto {
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
     private boolean deleted;
-    // private List<Comment> commentList = new ArrayList<>();
+    private List<ShowCommentResponseDto> commentList = new ArrayList<>();
 
     public ShowPostOneResponseDto(Post post) {
         this.id = post.getId();
@@ -27,6 +30,30 @@ public class ShowPostOneResponseDto {
         this.updatedAt = post.getUpdatedAt();
         this.deleted = post.isDeleted();
         // *** this.commentList = post.getComments(); 이렇게 작성했더니 게시글과 댓글이 무한 참조되는 상황이 발생
+        this.commentList = makeCommentList(post.getComments());
+    }
+
+    private List<ShowCommentResponseDto> makeCommentList(List<Comment> comments) {
+        for (Comment comment : comments) this.commentList.add(new ShowCommentResponseDto(comment));
+        return this.commentList;
+    }
+
+    @Getter
+    @Setter
+    private static class ShowCommentResponseDto {
+        private UUID id;
+        private String content;
+        private LocalDateTime createdAt;
+        private LocalDateTime updatedAt;
+        private boolean deleted;
+
+        private ShowCommentResponseDto(Comment comment) {
+            this.id = comment.getId();
+            this.content = comment.getContent();
+            this.createdAt = comment.getCreatedAt();
+            this.updatedAt = comment.getUpdatedAt();
+            this.deleted = comment.isDeleted();
+        }
     }
 
 }
