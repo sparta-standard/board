02768461b3b4287eb86f1ecb9c4204c22c3083f4
dydwo6018 <dydwo6018@gmail.com>
2025-04-02 10:59:53 +0900

    게시글 수정 기능 controller

--- src/main/java/io/sparta/board/controller/PostController.java
+++ src/main/java/io/sparta/board/controller/PostController.java
@@ -1,10 +1,10 @@
 package io.sparta.board.controller;
 
 import io.sparta.board.dto.PostCreateRequestDto;
-import io.sparta.board.dto.PostCreateResponseDto;
+import io.sparta.board.dto.PostResponseDto;
+import io.sparta.board.dto.PostUpdateRequestDto;
 import io.sparta.board.service.PostService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,18 +18,25 @@ public class PostController {
     private final PostService postService;
 
     @PostMapping
-    public ResponseEntity<PostCreateResponseDto> createPost(@RequestBody PostCreateRequestDto requestDto){
+    public ResponseEntity<PostResponseDto> createPost(@RequestBody PostCreateRequestDto requestDto){
         return ResponseEntity.ok(postService.createPost(requestDto));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<PostCreateResponseDto> getPost(@PathVariable UUID id){
+    public ResponseEntity<PostResponseDto> getPost(@PathVariable UUID id){
         return ResponseEntity.ok(postService.getPostById(id));
     }
 
     @GetMapping
-    private ResponseEntity <List<PostCreateResponseDto>> getAllPosts(){
+    private ResponseEntity <List<PostResponseDto>> getAllPosts(){
         return ResponseEntity.ok(postService.getAllPosts());
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PostResponseDto> updatePost(@PathVariable UUID id,
+                                                      @RequestBody PostUpdateRequestDto requestDto){
+        return ResponseEntity.ok(postService.updatePost(id, requestDto));
+    }
+
 
 }
