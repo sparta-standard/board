1f1e15a0ab2598e39cb42b8a4aefafd4161ef7e4
dydwo6018 <dydwo6018@gmail.com>
2025-04-02 10:34:00 +0900

    게시글 단건 조회 controller

--- src/main/java/io/sparta/board/controller/PostController.java
+++ src/main/java/io/sparta/board/controller/PostController.java
@@ -5,10 +5,9 @@ import io.sparta.board.dto.PostCreateResponseDto;
 import io.sparta.board.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/posts")
@@ -19,6 +18,11 @@ public class PostController {
     @PostMapping
     public ResponseEntity<PostCreateResponseDto> createPost(@RequestBody PostCreateRequestDto requestDto){
         return ResponseEntity.ok(postService.createPost(requestDto));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PostCreateResponseDto> getPost(@PathVariable UUID id){
+        return ResponseEntity.ok(postService.getPostById(id));
     }
 
 }
