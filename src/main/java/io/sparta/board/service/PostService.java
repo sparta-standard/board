a4684f409fe47394fb3da222efef487b2f0765fd
dydwo6018 <dydwo6018@gmail.com>
2025-04-02 10:59:46 +0900

    게시글 수정 기능 service

--- src/main/java/io/sparta/board/service/PostService.java
+++ src/main/java/io/sparta/board/service/PostService.java
@@ -1,7 +1,8 @@
 package io.sparta.board.service;
 
 import io.sparta.board.dto.PostCreateRequestDto;
-import io.sparta.board.dto.PostCreateResponseDto;
+import io.sparta.board.dto.PostResponseDto;
+import io.sparta.board.dto.PostUpdateRequestDto;
 import io.sparta.board.entity.Post;
 import io.sparta.board.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,26 +19,37 @@ public class PostService {
     private final PostRepository postRepository;
 
     @Transactional
-    public PostCreateResponseDto createPost(PostCreateRequestDto requestDto){
+    public PostResponseDto createPost(PostCreateRequestDto requestDto){
         Post post = new Post(requestDto.getTitle(),requestDto.getContent());
 
         Post savedPost = postRepository.save(post);
-        return new PostCreateResponseDto(savedPost);
+        return new PostResponseDto(savedPost);
     }
 
     @Transactional(readOnly = true)
-    public PostCreateResponseDto getPostById(UUID id){
+    public PostResponseDto getPostById(UUID id){
         Post post = postRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글을 찾을 수 없습니다."));
-        return new PostCreateResponseDto(post);
+        return new PostResponseDto(post);
     }
 
     @Transactional(readOnly = true)
-    public List<PostCreateResponseDto> getAllPosts(){
+    public List<PostResponseDto> getAllPosts(){
         List<Post> posts = postRepository.findAll();
         return posts.stream()
-                .map(PostCreateResponseDto::new)
+                .map(PostResponseDto::new)
                 .toList();
+    }
+
+    @Transactional
+    public PostResponseDto updatePost(UUID id, PostUpdateRequestDto requestDto){
+        Post post = postRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글을 찾을 수 없습니다."));
+
+        post.update(requestDto.getTitle(),requestDto.getContent());
+
+        return new PostResponseDto(post);
+
     }
 
 }
