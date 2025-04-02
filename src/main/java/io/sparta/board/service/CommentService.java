f445a7b5bb936203179cc7c40cc0f80e4c1e9548
dydwo6018 <dydwo6018@gmail.com>
2025-04-02 14:25:46 +0900

    CommentService update 기능 추가

--- src/main/java/io/sparta/board/service/CommentService.java
+++ src/main/java/io/sparta/board/service/CommentService.java
@@ -1,6 +1,7 @@
 package io.sparta.board.service;
 
 import io.sparta.board.dto.request.CommentCreateRequestDto;
+import io.sparta.board.dto.request.CommentUpdateRequestDto;
 import io.sparta.board.dto.response.CommentResponseDto;
 import io.sparta.board.entity.Comment;
 import io.sparta.board.entity.Post;
@@ -9,6 +10,8 @@ import io.sparta.board.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -27,5 +30,16 @@ public class CommentService {
         Comment savedComment = commentRepository.save(comment);
 
         return new CommentResponseDto(savedComment);
+    }
+
+    @Transactional
+    public CommentResponseDto updateComment(UUID id, CommentUpdateRequestDto requestDto){
+        Comment comment = commentRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("해당 댓글을 찾을 수 없습니다."));
+
+        comment.update(requestDto.getContent());
+
+        return new CommentResponseDto(comment);
+
     }
 }
