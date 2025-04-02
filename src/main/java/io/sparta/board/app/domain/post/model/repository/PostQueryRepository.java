368f6ba297438e342239a9dcfcb1c12134291341
adgjl <adgjl0122@naver.com>
2025-04-02 14:16:37 +0900

    feat: Post Read Query Repository 수정

--- src/main/java/io/sparta/board/app/domain/post/model/repository/PostQueryRepository.java
+++ src/main/java/io/sparta/board/app/domain/post/model/repository/PostQueryRepository.java
@@ -1,11 +1,12 @@
 package io.sparta.board.app.domain.post.model.repository;
 
+import static io.sparta.board.app.domain.comment.model.entity.QComment.comment;
 import static io.sparta.board.app.global.config.QueryDslConfig.getUsableSize;
 
 import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import io.sparta.board.app.domain.post.model.entity.Post;
+import io.sparta.board.app.domain.comment.model.entity.Comment;
 import io.sparta.board.app.global.config.QueryDslConfig;
 import java.util.List;
 import java.util.UUID;
@@ -15,23 +16,21 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
-import static io.sparta.board.app.domain.post.model.entity.QPost.post;
-
 @RequiredArgsConstructor
 @Repository
 public class PostQueryRepository {
 
 	private final JPAQueryFactory jpaQueryFactory;
 
-	public Page<Post> findPostCommentList(UUID id, Pageable pageable) {
+	public Page<Comment> findPostCommentList(UUID id, Pageable pageable) {
 
-		OrderSpecifier<?>[] orderSpecifiers = QueryDslConfig.getAllOrderSpecifierArr(pageable, post);
+		OrderSpecifier<?>[] orderSpecifiers = QueryDslConfig.getAllOrderSpecifierArr(pageable, comment);
 
-		List<Post> contents = jpaQueryFactory
-			.select(post)
-			.from(post)
+		List<Comment> contents = jpaQueryFactory
+			.selectFrom(comment)
 			.where(
-				post.id.eq(id)
+				comment.post.id.eq(id),
+				comment.deleted.eq(false)
 			)
 			.orderBy(orderSpecifiers)
 			.offset(pageable.getOffset())
@@ -39,10 +38,11 @@ public class PostQueryRepository {
 			.fetch();
 
 		JPAQuery<Long> countQuery = jpaQueryFactory
-			.select(post.count())
-			.from(post)
+			.select(comment.count())
+			.from(comment)
 			.where(
-				post.id.eq(id)
+				comment.post.id.eq(id),
+				comment.deleted.eq(false)
 			);
 
 		return PageableExecutionUtils.getPage(contents, pageable, countQuery::fetchOne);
