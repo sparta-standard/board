59773ef3af07f0c6050e33bc427ff21cfb7ad7e4
yejiscore <yejisong24@gmail.com>
2025-04-02 10:07:21 +0900

    feat: @AllArgsConstructor 어노테이션 추가

--- src/main/java/io/sparta/board/post/application/dto/request/PostRequestDto.java
+++ src/main/java/io/sparta/board/post/application/dto/request/PostRequestDto.java
@@ -1,12 +1,14 @@
 package io.sparta.board.post.application.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class PostRequestDto {
 
